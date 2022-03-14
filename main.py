5bdd28a412cbb0d42d04ea8916ab04402a763083
MohammedAamirShuaib <mohammed.shuaib@osganalytics.com>
2022-03-14 15:19:34 +0530

    Dockerized and running on Gunicorn

--- main.py
+++ main.py
@@ -27,17 +27,15 @@ templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 s3 = boto3.client('s3', aws_access_key_id=access_id, aws_secret_access_key=secret_id, region_name='us-east-1')
 
-
 @app.get('/', response_class=HTMLResponse)
 async def file_temp(request: Request):
   dir_name = str(uuid.uuid4())
-  return templates.TemplateResponse("webpage.html", {'request': request , 'dir_name': dir_name})
+  return templates.TemplateResponse("webpage.html", {'request': request , 'dir_name': dir_name, "start_time": str(time.time())})
 
-
-@app.post("/{dir_name}/", response_class=HTMLResponse)
-async def create_upload_files(*,files: List[UploadFile] = File(...), request: Request,dir_name):
-
+@app.post("/{start_time}/{dir_name}/", response_class=HTMLResponse)
+async def create_upload_files(*,files: List[UploadFile] = File(...), request: Request,dir_name,start_time):
   log_list = []
+  log_list.append("Start_time:&nbsp;"+datetime.now().strftime("%I:%M:%S")+"<br>")
 
   for file in files:
     if file.filename == "":
@@ -51,29 +49,33 @@ async def create_upload_files(*,files: List[UploadFile] = File(...), request: Re
       os.remove("documents/"+dir_name+".txt")
       return HTMLResponse(content=html_page_download(wc,dir_name), status_code=200)
 
-  os.makedirs('documents/'+dir_name)
+  os.makedirs("documents/"+dir_name, exist_ok=True)
   wc = ""
 
   log_list.append("Please find the logs for your transcription below:<br><br>")
-  log_list.append("Files Uploaded: <br><br>")
+  log_list.append("Files Uploaded: <br>")
 
   for i in files:
     log_list.append(i.filename+"<br>")
-
+  
+  log_list.append("<br><br>")
+  upload_finish_time= round(time.time(),2)
+  upload_time = str(round(upload_finish_time - float(start_time),2))
+  log_list.append("Time took to upload files to Ec2:&nbsp;&nbsp;"+upload_time+"<br><br>")
   for file in files:
     token = "d3b6f15c585b4a1bbf43f20f60535185"
     fname = file.filename
-
+    transcription_start_time = time.time()
     tid = upload_file(token,file.file)
     result = {}
-    log_list.append("starting to transcribe the file:"+fname+"<br>")
+    log_list.append("starting to transcribe the file: &nbsp;&nbsp;"+fname+"<br>")
     print('starting to transcribe the file: [ {} ]'.format(fname))
     while result.get("status") != 'completed':
         print(result.get("status"))
         result = get_text(token, tid)
         # Handeling Errors
         if result.get("status") == 'error':
-          log_list.append("Error occured while transcribing the file :"+fname+"<br>")
+          log_list.append("Error occured while transcribing the file :&nbsp;&nbsp;"+fname+"<br>")
           wc = wc+"<img src='/static/error.jpg' height='20'>&nbsp;&nbsp;Error &nbsp;:&nbsp;"+fname+"<br><br>"
           shutil.make_archive("documents/"+dir_name,"zip","documents/"+dir_name)
           logs= "".join(log_list)
@@ -87,15 +89,18 @@ async def create_upload_files(*,files: List[UploadFile] = File(...), request: Re
           shutil.rmtree("documents/"+dir_name)
           return HTMLResponse(content=html_page_download(wc,dir_name), status_code=200)
 
-    log_list.append("Transcription Completed for the file:"+fname+"<br>")
+    transcription_finish_time = time.time()
+    transcribe_time= str(round(transcription_finish_time-transcription_start_time,2))
+    log_list.append("Transcription Completed for the file:&nbsp;&nbsp;"+fname+"&nbsp; in "+transcribe_time+"<br>")
+
     df = json_data_extraction(result,fname)
     print('saving transcript...')
-    log_list.append("Saving the transcription of the file :"+fname+"<br>")
+    log_list.append("Saving the transcription of the file :&nbsp;&nbsp;"+fname+"<br>")
 
     df = df[['spcode','utter']]
 
     print('Converting files')
-    log_list.append("Converting into document file:"+fname+"<br>")
+    log_list.append("Converting into document file:&nbsp;&nbsp;"+fname+"<br>")
 
     # open an existing document
     doc = docx.Document()
@@ -112,7 +117,7 @@ async def create_upload_files(*,files: List[UploadFile] = File(...), request: Re
             t.cell(i+1,j).text = str(df.values[i,j])
 
     print('saving transcript...')
-    log_list.append("Saving the document file:"+fname+"<br><br><br>")
+    log_list.append("Saving the document file:&nbsp;&nbsp;"+fname+"<br><br><br>")
     doc.save("documents/"+dir_name+"/"+fname+".docx")
     wc = wc+"<img src='/static/success.jpg' height='20'>&nbsp;&nbsp;Completed &nbsp;:&nbsp;"+fname+"<br><br>"
 
@@ -138,4 +143,4 @@ async def download(dir_name):
 async def logs(dir_name):
   logobj = s3.get_object(Bucket = "transcription-documents", Key = "logs/"+dir_name+".txt")
   log_content = logobj["Body"].read().decode("utf-8")
-  return HTMLResponse(content=html_page_logs(log_content,dir_name), status_code=200)
+  return HTMLResponse(content=html_page_logs(log_content), status_code=200)
